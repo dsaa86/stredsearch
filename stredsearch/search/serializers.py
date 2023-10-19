28a54658f6cc59a3f4bed34f8f00ea55eb83b3e6
Matthew Dunn <mattjohndunn@gmail.com>
2023-10-19 12:16:42 +0400

    Built out serializer classes for each Stack model

--- stredsearch/search/serializers.py
+++ stredsearch/search/serializers.py
@@ -5,20 +5,18 @@ class StackQuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = StackQuestion
         fields = '__all__'
-    # created_on_stredsearch = serializers.DateTimeField(read_only=True)
-    # updated_on_stredsearch = serializers.DateTimeField(read_only=True)
-    # is_answered = serializers.BooleanField(read_only=True)
-    # view_count = serializers.IntegerField(read_only=True)
-    # answer_count = serializers.IntegerField(read_only=True)
-    # score = serializers.IntegerField(read_only=True)
-    # last_activity_date = serializers.DateTimeField(read_only=True)
-    # creation_date = serializers.DateTimeField(read_only=True)
-    # last_edit_date = serializers.DateTimeField(read_only=True)
-    # question_id = serializers.IntegerField(read_only=True)
-    # link = serializers.CharField(read_only=True)
-    # title = serializers.CharField(read_only=True)
 
 class StackUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = StackUser
+        fields = '__all__'
+
+class StackRouteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StackRoute
+        fields = '__all__'
+
+class StackMetaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StackMeta
         fields = '__all__'
