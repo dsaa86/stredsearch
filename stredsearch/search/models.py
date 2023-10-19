5d56f83af637faa62e0add35c6bb960bf6db775f
Matthew Dunn <mattjohndunn@gmail.com>
2023-10-19 12:12:54 +0400

    Starting to put together basic REST framework

--- stredsearch/search/models.py
+++ stredsearch/search/models.py
@@ -2,23 +2,25 @@ from django.db import models
 
 # Create your models here.
 
-class stackRoute(models.Model):
+class StackRoute(models.Model):
     route_root = models.CharField(blank=False, default='', max_length=200)
     route = models.CharField(blank=False, default='', max_length=200)
     params = models.JSONField()
 
-class stackMeta(models.Model):
+class StackMeta(models.Model):
     route_prepend = models.CharField(max_length=200)
     route_append = models.CharField(max_length=200)
     filters = models.JSONField()
     sort = models.JSONField()
 
-class stackUser(models.Model):
+class StackUser(models.Model):
     user_id = models.IntegerField()
     display_name = models.CharField(max_length=200)
 
-class stackQuestion(models.Model):
-    owner = models.ForeignKey(stackUser, on_delete=models.PROTECT)
+class StackQuestion(models.Model):
+    created_on_stredsearch = models.DateTimeField(auto_now_add=True)
+    updated_on_stredsearch = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(StackUser, on_delete=models.PROTECT)
     is_answered = models.BooleanField()
     view_count = models.IntegerField()
     answer_count = models.IntegerField()
--- stredsearch/search/views.py
+++ stredsearch/search/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from search.models import *
+from search.serializers import *
 
-# Create your views here.
+@api_view(['GET'])
+def questionList(request):
+    if request.method == 'GET':
+        questions = StackQuestion.objects.all()
+        serializer = StackQuestionSerializer(questions, many=True)
+        return Response(serializer.data)
