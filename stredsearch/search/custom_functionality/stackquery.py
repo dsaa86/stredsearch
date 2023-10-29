e4caaffcfafc6bb08427910b8f041c5006ad9af9
Matthew Dunn <mattjohndunn@gmail.com>
2023-10-29 19:11:43 +0400

    Tidied up models and refactored to include Stack routing and parameters.

--- stredsearch/search/custom_functionality/stackquery.py
+++ stredsearch/search/custom_functionality/stackquery.py
@@ -4,33 +4,35 @@ from datetime import datetime
 import zoneinfo
 from django.utils.dateparse import parse_datetime
 
-ACCESS_ROUTES = {
-    "meta": {
-        "route_prepend": "https://api.stackexchange.com",
-        "route_append": {
-            "site": "stackoverflow",
-        },
-        "filters": {
-            "fromdate": "filter by oldest age of question",
-            "todate": "filter by youngest age of question",
-            "min": "filter by youngest possible age of question",
-            "max": "filter by oldest possible age of question",
-            "page": "the page number of results to show",
-            "pagesize": "the number of results per page (max 100)",
-            "order": {
-                "asc": "order by ascending order",
-                "desc": "order by descending order",
-            },
-            "sort": {
-                "activity": "sort by recent activity",
-                "votes": "sort by number of votes",
-                "creation": "sort by creation date",
-                "hot": "sort by current popularity of question",
-                "week": "sort by creation week",
-                "month": "sort by creation month",
-            },
-        },
+META_DATA = {
+    "route_prepend": "https://api.stackexchange.com",
+    "route_append": {
+        "site": "stackoverflow",
     },
+}
+
+FILTERS = {
+    "fromdate": "filter by oldest age of question",
+    "todate": "filter by youngest age of question",
+    "min": "filter by youngest possible age of question",
+    "max": "filter by oldest possible age of question",
+    "page": "the page number of results to show",
+    "pagesize": "the number of results per page (max 100)",
+    "order": {
+        "asc": "order by ascending order",
+        "desc": "order by descending order",
+    },
+    "sort": {
+        "activity": "sort by recent activity",
+        "votes": "sort by number of votes",
+        "creation": "sort by creation date",
+        "hot": "sort by current popularity of question",
+        "week": "sort by creation week",
+        "month": "sort by creation month",
+    },
+}
+
+ACCESS_ROUTES = {
     "questions": {
         "question_by_tag": {
             "route": "/2.3/questions",
--- stredsearch/search/models.py
+++ stredsearch/search/models.py
@@ -1,37 +1,63 @@
 from django.db import models
 
 
+class StackParams(models.Model):
+    param_name = models.CharField(max_length=50, blank=False)
+    param_description = models.CharField(max_length=150, blank=False)
+    def __str__(self) -> str:
+        return self.param_name
+
+class StackSortMethods(models.Model):
+    sort_name = models.CharField(max_length=50, blank=False)
+    sort_description = models.CharField(max_length=150, blank=False)
+    def __str__(self) -> str:
+        return self.sort_name
+
+class StackOrderMethods(models.Model):
+    order_name = models.CharField(max_length=50, blank=False)
+    order_description = models.CharField(max_length=150, blank=False)
+    def __str__(self) -> str:
+        return self.order_name
+
+class StackFilters(models.Model):
+    filter_name = models.CharField(max_length=50, blank=False)
+    filter_description = models.CharField(max_length=150, blank=False)
+    def __str__(self) -> str:
+        return self.filter_name
+    
+class StackQuestionDataFields(models.Model):
+    data_field_name = models.CharField(max_length=50, blank=False)
+    data_field_description = models.CharField(max_length=150, blank=False)
+    def __str__(self) -> str:
+        return self.data_field_name
+
+
 class StackRoute(models.Model):
-    route_root = models.CharField(blank=False, default="", max_length=200)
+    route_category = models.CharField(blank=False, default="", max_length=200)
     route = models.CharField(blank=False, default="", max_length=200)
-    params = models.JSONField()
+    params = models.ManyToManyField(StackParams, blank=True)
 
 
-class StackMeta(models.Model):
+class StackRouteMeta(models.Model):
     route_prepend = models.CharField(max_length=200)
     route_append = models.CharField(max_length=200)
-    filters = models.JSONField()
-    sort = models.JSONField()
 
 
 class StackUser(models.Model):
     user_id = models.IntegerField()
     display_name = models.CharField(max_length=200)
-
     def __str__(self) -> str:
         return self.display_name
 
 
 class StackTags(models.Model):
     tag_name = models.CharField(max_length=100, blank=False)
-
     def __str__(self) -> str:
         return self.tag_name
 
 
 class StackSearchTerms(models.Model):
     search_term = models.CharField(max_length=500, null=True)
-
     def __str__(self) -> str:
         return self.search_term
 
@@ -43,6 +69,8 @@ class StredSearchQuestion(models.Model):
     link = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
     times_returned_as_search_result = models.IntegerField(default=0)
+    def getTitle(self):
+        return self.title
 
 
 class StackQuestion(StredSearchQuestion):
@@ -56,10 +84,9 @@ class StackQuestion(StredSearchQuestion):
     last_edit_date = models.DateTimeField(null=True)
     question_id = models.IntegerField()
     tags = models.ManyToManyField(StackTags, blank=True)
-
     def __str__(self) -> str:
-        return self.title
-
+        return super.getTitle(self)
+    
 
 class RedditQuestion(StredSearchQuestion):
     type = models.CharField(max_length=10)
