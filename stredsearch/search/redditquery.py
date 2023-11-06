6419269f78cd4f70adc5ce4574fd57f9950a7fe5
Matthew Dunn <mattjohndunn@gmail.com>
2023-11-06 11:44:01 +0400

    Refactored redditquery processTermsForURL

--- stredsearch/search/redditquery.py
+++ stredsearch/search/redditquery.py
@@ -1,20 +1,31 @@
 import time
+from tabnanny import check
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+def checkObjAndRaiseTypeError(test_obj: object, test_type, error_msg: str):
+    if not isinstance(test_obj, test_type):
+        raise TypeError(error_msg)
+
+
+def checkStringAndRaiseValueError(test_string: str, test_value: str, error_msg: str):
+    if test_string == test_value:
+        raise ValueError(error_msg)
 
 
 def buildTermsFromParams(q: str, search_type: str, limit: str) -> dict:
 
-    if not isinstance(q, str):
-        raise TypeError("q must be of type str")
-    if not isinstance(search_type, str):
-        raise TypeError("search_type must be of type str")
-    if not isinstance(limit, str):
-        raise TypeError("limit must be of type str")
+    checkObjAndRaiseTypeError(q, str, "q must be of type str")
+    checkObjAndRaiseTypeError(search_type, str, "search_type must be of type str")
+    checkObjAndRaiseTypeError(limit, str, "limit must be of type str")
 
     return {
         "q": f"{q}",
@@ -25,9 +36,8 @@ def buildTermsFromParams(q: str, search_type: str, limit: str) -> dict:
 
 def buildSubredFromParams(subreds: str) -> list:
 
-    if not isinstance(subreds, str):
-        raise TypeError("subreds must be of type str")
-    
+    checkObjAndRaiseTypeError(subreds, str, "subreds must be of type str")
+
     subreds = subreds.replace(" ", "")
 
     if split_list := subreds.split(","):
@@ -36,46 +46,19 @@ def buildSubredFromParams(subreds: str) -> list:
         raise ValueError("subreds must contain at least one subreddit")
 
 
-# def parseRedditSearchResults(search_results: dict) -> list:
-
-
-#     if not isinstance(search_results, dict):
-#         raise TypeError("search_results must be of type dict")
-    
-
-#     formatted_results_set = []
-
-#     for tuple in search_results:
-#         result = {"title": f"{tuple[0]}", "link": f"https://reddit.com{tuple[1]},"}
-#         formatted_results_set.append(result)
-
-#     return formatted_results_set
-
-
 def buildUrl(terms: dict, subreddit: str = '') -> str:
 
-    if not isinstance(terms, dict):
-        raise TypeError("terms must be of type dict")
-    if not isinstance(subreddit, str):
-        raise TypeError("subreddit must be of type str")
+    checkObjAndRaiseTypeError(terms, dict, "terms must be of type dict")
+    checkObjAndRaiseTypeError(subreddit, str, "subreddit must be of type str")
     
-    if subreddit == '':
-        raise ValueError("subreddit must not be empty string")
+    checkStringAndRaiseValueError(subreddit, '', "subreddit must not be empty string")
+    checkStringAndRaiseValueError(terms["q"], '', "terms must contain a q parameter that is not empty string")
+    checkStringAndRaiseValueError(terms["type"], '', "terms must contain a type parameter that is not empty string")
+    checkStringAndRaiseValueError(terms["limit"], '', "terms must contain a limit parameter that is not empty string")
+    checkStringAndRaiseValueError(terms["q"], None, "terms must contain a q parameter that is not of type None")
+    checkStringAndRaiseValueError(terms["type"], None, "terms must contain a type parameter that is not of type None")
+    checkStringAndRaiseValueError(terms["limit"], None, "terms must contain a limit parameter that is not of type None")
     
-    if terms["q"] == '':
-        raise ValueError("terms must contain a q parameter")
-    if terms["type"] == '':
-        raise ValueError("terms must contain a type parameter")
-    if terms["limit"] == '':
-        raise ValueError("terms must contain a limit parameter")
-    
-    if terms["q"] is None:
-        raise ValueError("terms must contain a q parameter")
-    if terms["type"] is None:
-        raise ValueError("terms must contain a type parameter")
-    if terms["limit"] is None:
-        raise ValueError("terms must contain a limit parameter")
-
     processed_terms = processTermsForUrl(terms)
 
     return f"https://www.reddit.com{subreddit}{processed_terms}"
@@ -83,21 +66,11 @@ def buildUrl(terms: dict, subreddit: str = '') -> str:
 
 def processTermsForUrl(terms: dict) -> str:
 
-    if not isinstance(terms, dict):
-        raise TypeError("terms must be of type dict")
+    checkObjAndRaiseTypeError(terms, dict, "terms must be of type dict")
 
-    last_key_in_dict = list(terms)[-1]
+    terms_list = [f"{key}={term}" for key, term in terms.items()]
+    return '/search?' '&'.join(terms_list)
 
-    processed_terms = '/search?'
-
-    for key, term in terms.items():
-        term_string = f"{key}={term}"
-        
-        if key != last_key_in_dict:
-            term_string = f"{term_string}&"
-        processed_terms = processed_terms + term_string
-
-    return processed_terms
 
 
 def parseLinksFromHtml(html: any) -> list:
@@ -113,8 +86,7 @@ def parseLinksFromHtml(html: any) -> list:
 
 def generateRedditLinkDicts(extracted_tags: list) -> list:
 
-    if not isinstance(extracted_tags, list):
-        raise TypeError("extracted_tags must be of type list")
+    checkObjAndRaiseTypeError(extracted_tags, list, "extracted_tags must be of type list")
     
     links = []
     for tag in extracted_tags:
@@ -126,14 +98,10 @@ def generateRedditLinkDicts(extracted_tags: list) -> list:
 
 def getRedditHTMLViaSelenium(url: str) -> str:
 
-    if not isinstance(url, str):
-        raise TypeError("url must be of type str")
-    
-    if url == '':
-        raise ValueError("url must not be empty string")
-    
-    if url == " ":
-        raise ValueError("url must not be whitespace")
+    checkObjAndRaiseTypeError(url, str, "url must be of type str")
+
+    checkStringAndRaiseValueError(url, '', "url must not be empty string")
+    checkStringAndRaiseValueError(url, " ", "url must not be whitespace")
 
     options = Options()
     options.add_argument("--headless=new")
@@ -141,36 +109,29 @@ def getRedditHTMLViaSelenium(url: str) -> str:
     driver = webdriver.Chrome(options = options)
     driver.get(url)
 
-    time.sleep(2)
+    try:
+        WebDriverWait(driver,10).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
+    except TimeOutException:
+        print("TimeoutException: Failed to load page")
 
     return driver.page_source
 
 
 def searchRedditAndReturnResponse(q: str, search_type: str, limit: str, subreddits: str) -> dict:
 
-    if not isinstance(q, str):
-        raise TypeError("q must be of type str")
-    if not isinstance(search_type, str):
-        raise TypeError("search_type must be of type str")
-    if not isinstance(limit, str):
-        raise TypeError("limit must be of type str")
-    if not isinstance(subreddits, str):
-        raise TypeError("subreddits must be of type str")
-    
-    if q == '':
-        raise ValueError("q must not be empty string")
-    if search_type == '':
-        raise ValueError("search_type must not be empty string")
-    if limit == '':
-        raise ValueError("limit must not be empty string")
-    
-    if q == " ": 
-        raise ValueError("q must not be whitespace")
-    if search_type == " ":
-        raise ValueError("search_type must not be whitespace")
-    if limit == " ":
-        raise ValueError("limit must not be whitespace")
+    checkObjAndRaiseTypeError(q, str, "q must be of type str")
+    checkObjAndRaiseTypeError(search_type, str, "search_type must be of type str")
+    checkObjAndRaiseTypeError(limit, str, "limit must be of type str")
+    checkObjAndRaiseTypeError(subreddits, str, "subreddits must be of type str")
 
+    checkStringAndRaiseValueError(q, '', "q must not be empty string")
+    checkStringAndRaiseValueError(search_type, '', "search_type must not be empty string")
+    checkStringAndRaiseValueError(limit, '', "limit must not be empty string")
+    
+    checkStringAndRaiseValueError(q, " ", "q must not be whitespace")
+    checkStringAndRaiseValueError(search_type, " ", "search_type must not be whitespace")
+    checkStringAndRaiseValueError(limit, " ", "limit must not be whitespace")
+    
     search_terms = buildTermsFromParams(q, search_type, limit)
 
     subreddit_list = buildSubredFromParams(subreddits)
