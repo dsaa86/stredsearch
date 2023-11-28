c642b47b142d82217dc3f9d00e5e2f623bffebc8
Matthew Dunn <mattjohndunn@gmail.com>
2023-11-29 01:41:10 +0400

    Adjusted views and serializers to optimise user registration

--- stredsearch/search/views.py
+++ stredsearch/search/views.py
@@ -39,6 +39,21 @@ class RegisterUserView(generics.CreateAPIView):
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid(raise_exception=False):
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED, headers=headers
+            )
+        else:
+            print(serializer.errors)
+            errors = serializer.errors
+            if ("email" in errors) or ("username" in errors):
+                return Response(errors, status=status.HTTP_409_CONFLICT)
+            return Response(errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class GetStackOverflowQuestionsByTag(APIView):
     def checkObjAndRaiseTypeError(self, test_obj: object, test_type, error_msg: str):
