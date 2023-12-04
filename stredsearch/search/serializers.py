f4391c2d9f7b51071cb0ba4e60bb8e7432ca7903
Matthew Dunn <mattjohndunn@gmail.com>
2023-12-04 08:42:10 +0400

    Shutdown commit

--- stredsearch/search/serializers.py
+++ stredsearch/search/serializers.py
@@ -11,7 +11,7 @@ from search.models import *
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "email", "password"]
+        fields = ["username"]
 
 
 class RegisterSerializer(serializers.ModelSerializer):
